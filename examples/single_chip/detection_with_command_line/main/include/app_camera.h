80b98c56118f7152aff65ea5a43cc833ca98db06
diablo.fan <diablo.fan@libratone.com.cn>
2019-04-12 12:34:43 +0800

    merge ESP-EYE Pin map from camera_web_server

--- examples/single_chip/detection_with_command_line/main/include/app_camera.h
+++ examples/single_chip/detection_with_command_line/main/include/app_camera.h
@@ -52,6 +52,24 @@
  */
 #define CAMERA_FRAME_SIZE FRAMESIZE_QVGA
 
+#ifdef CONFIG_CAMERA_MODEL_ESP_EYE
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define Y2_GPIO_NUM       34
+#define Y3_GPIO_NUM       13
+#define Y4_GPIO_NUM       14
+#define Y5_GPIO_NUM       35
+#define Y6_GPIO_NUM       39
+#define Y7_GPIO_NUM       38
+#define Y8_GPIO_NUM       37
+#define Y9_GPIO_NUM       36
+#define XCLK_GPIO_NUM     4
+#define PCLK_GPIO_NUM     25
+#define HREF_GPIO_NUM     27
+#define VSYNC_GPIO_NUM    5
+#define SIOD_GPIO_NUM     18
+#define SIOC_GPIO_NUM     23
+#else
 #define PWDN_GPIO_NUM     0
 #define RESET_GPIO_NUM    2
 #define Y2_GPIO_NUM       4
@@ -68,6 +86,7 @@
 #define VSYNC_GPIO_NUM    25
 #define SIOD_GPIO_NUM     26
 #define SIOC_GPIO_NUM     27
+#endif
 
 #define XCLK_FREQ       20000000
 
